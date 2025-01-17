d2dd6b08b90ee1b603d58f5aa9e15c90a93fb47e
Giorgiod91 <giorgio.dettmar@gmx.de>
2025-01-17 12:34:59 +0100

    create model

--- tensorflowLearn.py
+++ tensorflowLearn.py
@@ -1,15 +1,27 @@
 import tensorflow as tf
-from tensorflow.python.keras.layers import Input, Dense
+
 import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
 
 
 
 
-x= np.array([[200.0, 17.0]])
+x= np.array([[200.0, 17.0],[120.0, 5.0],[425.0, 20.0], [212.0 ,18.0]])
+y= np.array([1,0,0,1])
 #dense is a other name for layers 
 layer_1 = Dense(units=3, activation="sigmoid")
 a1 = layer_1(x)
 
 layer_2 = Dense(units=1, activation="sigmoid")
 a2 = layer_2(a1)
+
+#string them together(2 layers) to build the neural network
+model = Sequential([layer_1, layer_2])
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(x,y)
+
+predictions = model.predict(x)
+print(predictions)
